482fa37a45958c0f857436dcc4372cdb3c46ea05
satoki252595 <satoki252595@gmail.com>
2024-04-29 10:34:53 +0900

    readmeへの説明追加

--- cli/api/nhkInfo.go
+++ cli/api/nhkInfo.go
@@ -44,34 +44,3 @@ func GetNhkRadio() []model.NhkRadio {
 	}
 	return nhkRadioList
 }
-
-// func GetNhkRadioInfo() model.NhkRadio {
-
-// 	var h model.NhkRadio
-
-// 	// http.Getを用いて外部APIを呼び出す
-// 	resp, err := http.Get("https://www.nhk.or.jp/radioondemand/json/index_v3/index.json")
-// 	// エラーハンドリング
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	} else {
-// 		// io.ReadAllでレスポンスボディを全て読み取る
-// 		data, _ := io.ReadAll(resp.Body)
-
-// 		var responseObject model.DateLists
-// 		// json.UnmarshalでJSONデータをGoのオブジェクトに変換する
-// 		json.Unmarshal(data, &responseObject)
-
-// 		m := make(map[string]struct{})
-
-// 		for _, p := range responseObject.DataList {
-// 			if _, ok := m[p.SiteID]; !ok {
-// 				h.ProgramName
-// 				m[p.SiteID] = struct{}{}
-// 				tmpList := []string{p.SiteID, p.ProgramName}
-// 				newList = append(newList, tmpList)
-// 			}
-
-// 		}
-// 	}
-// 	return newList
